e490629391b682c112ae1da3ed810c00dbf4dd04
Diego <a.b.diego04@gmail.com>
2016-11-14 18:45:39 -0600

    .

--- Operadores/src/mx/com/everis/tallerjava/operadores/Operadores.java
+++ Operadores/src/mx/com/everis/tallerjava/operadores/Operadores.java
@@ -1,5 +1,24 @@
 package mx.com.everis.tallerjava.operadores;
 
 public class Operadores {
+	
+	int valor1 = 11;
+	byte edad = 22;
+	String nombre = "Diego";
+	boolean boolean1 = true;
+	char sexom = 'M';
+	char sexoh = 'H';
+	byte mayor = 18;
+	
+	public void main(String args[]){
+		if (edad > mayor) {
+			datos();
+		}
+	}
+	
+	public void datos(){
+		
+		System.out.println(nombre+""+edad+""+sexoh+"");
+	}
 
 }
