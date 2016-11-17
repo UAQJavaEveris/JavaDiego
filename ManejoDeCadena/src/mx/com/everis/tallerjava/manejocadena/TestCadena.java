361d0d0aa65ea32d79322280ab3297ffe67b09ba
Diego <a.b.diego04@gmail.com>
2016-11-16 19:20:45 -0600

    .

--- ManejoDeCadena/src/mx/com/everis/tallerjava/manejocadena/TestCadena.java
+++ ManejoDeCadena/src/mx/com/everis/tallerjava/manejocadena/TestCadena.java
@@ -22,6 +22,25 @@ public class TestCadena {
 		}else{
 			System.out.println("No son iguales");
 		}
+		
+		System.out.println("------------------------------");
+		
+		String textcomplet = "Existen dos tipos de personas en el mundo";
+		
+		String[] text = textcomplet.split(" ");
+		//String[] text = textcomplet.split("\\s");
+		//System.out.println(text[1]);
+		
+		for (int i = 0; i < text.length; i++) {
+			System.out.println(text[i]);
+		}
+		
+		System.out.println("-----------------------");
+		
+		String text2 = textcomplet.trim();
+		System.out.println(text2);
+		
+		System.out.println("Posicion : "+text2.indexOf("p"));
 	}
 
 }
