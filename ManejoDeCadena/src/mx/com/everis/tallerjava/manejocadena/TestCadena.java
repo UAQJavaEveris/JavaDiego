855c01eff315af75543178d676b264a02751a512
Diego <a.b.diego04@gmail.com>
2016-11-16 19:05:45 -0600

    .

---
+++ ManejoDeCadena/src/mx/com/everis/tallerjava/manejocadena/TestCadena.java
@@ -0,0 +1,22 @@
+package mx.com.everis.tallerjava.manejocadena;
+
+public class TestCadena {
+	
+	public static void main(String[] args){
+		String nombre = "Diego";
+		String nombre2 = new String ("Diego");
+		
+		if (nombre == nombre2) {
+			System.out.println("Son iguales");
+		}else{
+			System.out.println("No son iguales");
+		}
+		
+		if (nombre.equals(nombre2)) {
+			System.out.println("Son iguales");
+		}else{
+			System.out.println("No son iguales");
+		}
+	}
+
+}
